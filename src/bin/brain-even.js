19243da41a38f6583996b8af60c7095b9d2fbb76
recroot <recroot89@gmail.com>
2017-08-06 00:23:37 +0500

    step8 + nmpignore

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -5,3 +5,24 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to brain games!');
 const actual = readlineSync.question('May I have your name?: ');
 console.log(`Hello, ${actual}!`);
+
+let acc = 0;
+while (acc < 3) {
+  const num = Math.round(Math.random() * 100);
+  console.log(`Question: ${num}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  const correctAnswer = num % 2 === 0 ? 'yes' : 'no';
+
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    acc += 1;
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    break;
+  }
+}
+if (acc === 3) {
+  console.log(`Congratulations, ${actual}!`);
+} else {
+  console.log(`Let's try again, ${actual}!`);
+}
